260862ced521934733e1d077d32768c59543cc56
brunoladislau <bruno.ladislau@bandtec.com.br>
2020-09-06 19:08:24 -0300

    Repositório da primeira avaliação integrada

---
+++ primeira-integrada/src/main/java/com/bandtec/primeiraintegrada/PrimeiraIntegradaApplication.java
@@ -0,0 +1,13 @@
+package com.bandtec.primeiraintegrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraIntegradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraIntegradaApplication.class, args);
+	}
+
+}
---
+++ primeira-integrada/src/test/java/com/bandtec/primeiraintegrada/PrimeiraIntegradaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandtec.primeiraintegrada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraIntegradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
